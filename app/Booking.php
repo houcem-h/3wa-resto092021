91986f6c24512e5fdaf9e342231f4229d90d872a
Houcem Hedhly <hedhoucem@gmail.com>
2021-09-06 15:48:00 +0100

    booking model & migration

---
+++ app/Booking.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    // protected $table = 'my_bookings';
+    // protected $primaryKey = 'CIN';
+    // public $incrementing = false;
+    // public $timestamps = false;
+}
