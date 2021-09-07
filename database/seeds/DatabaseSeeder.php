a0a1f97ed0880e73aeca64669453622635f0a222
Houcem Hedhly <hedhoucem@gmail.com>
2021-09-07 14:52:38 +0100

    seeding db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(BookingsTableSeeder::class);
+        $this->call(MealsTableSeeder::class);
+        $this->call(OrdersTableSeeder::class);
+        $this->call(OrderLinesTableSeeder::class);
     }
 }
