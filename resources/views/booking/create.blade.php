7599738c4ffb38e1490dbabab94b458e61ef00bf
Houcem Hedhly <hedhoucem@gmail.com>
2021-09-09 09:29:42 +0100

    create booking form old values

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -9,7 +9,7 @@
                 <div class="col">
                     <div class="form-group">
                       <label for="booking_date">Booking Date</label>
-                        <input type="date" name="booking_date" id="booking_date" class="form-control @error('booking_date'){{ 'is-invalid' }}@enderror" placeholder="" >
+                        <input type="date" name="booking_date" id="booking_date" class="form-control @error('booking_date'){{ 'is-invalid' }}@enderror" value="{{ old('booking_date') }}" >
                         @error('booking_date')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
@@ -18,7 +18,7 @@
                 <div class="col">
                     <div class="form-group">
                       <label for="booking_time">Booking time</label>
-                      <input type="time" name="booking_time" id="booking_time" class="form-control @error('booking_time'){{ 'is-invalid' }}@enderror" placeholder="" >
+                      <input type="time" name="booking_time" id="booking_time" class="form-control @error('booking_time'){{ 'is-invalid' }}@enderror" value="{{ old('booking_time') }}" >
                       @error('booking_time')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
@@ -27,7 +27,7 @@
                 <div class="col">
                     <div class="form-group">
                       <label for="seats_nbr">Number of seats</label>
-                      <input type="number" name="number_of_seats" min="1" max="20" id="seats_nbr" class="form-control @error('number_of_seats'){{ 'is-invalid' }}@enderror" placeholder="" >
+                      <input type="number" name="number_of_seats" min="1" max="20" id="seats_nbr" class="form-control @error('number_of_seats'){{ 'is-invalid' }}@enderror" value="{{ old('number_of_seats') }}" >
                       @error('number_of_seats')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
